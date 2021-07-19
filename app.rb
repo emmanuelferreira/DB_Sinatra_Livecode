3204bd0feeb7302d5f78b880e4ed8a4991876974
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2021-07-19 16:42:32 +0200

    livecode 19/07/21

--- app.rb
+++ app.rb
@@ -2,7 +2,26 @@ require_relative "config/application"
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
+require "pry-byebug"
 
 get "/" do
-  "Hello world! :)"
+  @restaurants = Restaurant.all
+
+  erb :index
+end
+
+get "/restaurants/:id" do
+  restaurant_id = params[:id]
+  @restaurant = Restaurant.find(restaurant_id)
+
+  erb :show
+end
+
+post "/restaurants" do
+  restaurant_name = params[:name]
+  restaurant_address = params[:address]
+  binding.pry
+  Restaurant.create!(name: restaurant_name, address: restaurant_address)
+
+  redirect "/"
 end
